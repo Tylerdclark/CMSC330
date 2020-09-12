f7844577c7eb525edf92e65999ef455d046ea493
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-12 19:44:48 -0400

    This works for now, but will try to make it look better.

--- project1/src/LexicalAnalyzer.java
+++ project1/src/LexicalAnalyzer.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class LexicalAnalyzer {
     private final StreamTokenizer tokenizer;
     private Reader reader;
+    private int tokenCounter = 0;
 
     public LexicalAnalyzer(File currentFile) {
         try {
@@ -15,27 +16,60 @@ public class LexicalAnalyzer {
         tokenizer.ordinaryChar('.');
         tokenizer.quoteChar('"');
     }
-    // chaning Token to void for testing
-    public void getNext() throws IOException {
-        //        switch (tokenizer.nextToken()){
-        //
-        //        }
+
+    public String getStringValue() {
+        return tokenizer.sval;
+    }
+
+    public double getNumberValue() {
+        return tokenizer.nval;
+    }
+
+    public int getLineNumber() {
+        return tokenizer.lineno();
+    }
+    public int getTokenCounter(){
+        return this.tokenCounter;
+    }
+
+    public Token getNext() throws IOException {
         int nextToken = tokenizer.nextToken();
-        while (nextToken != StreamTokenizer.TT_EOF) {
+        tokenCounter++;
+        System.out.println(tokenizer);
+        switch (nextToken) {
+            case StreamTokenizer.TT_NUMBER:
+                return Token.NUMBER;
+            case StreamTokenizer.TT_WORD:
+                for (Token token : Token.values()) {
+                    if (token.name().equals(tokenizer.sval.toUpperCase())) {
+                        return token;
+                    }
+                }
+            case '"':
+                return Token.STRING;
+            case StreamTokenizer.TT_EOF:
+                return Token.EOF;
+            default:
+                return getPunctuationToken(tokenizer.ttype);
+        }
+    }
 
-            if (nextToken == StreamTokenizer.TT_NUMBER) {
-                System.out.println("Number: "+tokenizer.nval + " TTYPE: "+ tokenizer.ttype);
-            } else if (nextToken == StreamTokenizer.TT_WORD) {
-                System.out.println("String: " + tokenizer.sval+ " TTYPE: "+ tokenizer.ttype);
-
-            } else if(nextToken == '"'){
-                System.out.println("String: " + tokenizer.sval+" TTYPE: "+ tokenizer.ttype);
-            }else {
-
-                System.out.println((char)nextToken);
-            }
-
-            nextToken = tokenizer.nextToken();
+    private Token getPunctuationToken(int ttype) {
+        switch (ttype) {
+            case 40:
+                return Token.LEFT_PAREN;
+            case 41:
+                return Token.RIGHT_PAREN;
+            case 44:
+                return Token.COMMA;
+            case 46:
+                return Token.PERIOD;
+            case 58:
+                return Token.COLON;
+            case 59:
+                return Token.SEMICOLON;
+            default:
+                return Token.UNKNOWN;
         }
     }
 }
