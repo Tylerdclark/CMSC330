ae41a7d2e4d4b6897eef018059f84c0623c30a44
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-11 21:31:27 -0400

    Better to put it into it's own class

--- project1/src/FileHandler.java
+++ project1/src/FileHandler.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class FileHandler {
+import java.io.File;
+import java.util.Objects;
+
+public class FileHandler {
+
+  private File[] files;
+
+  public FileHandler(String[] args) {
+
+    if (args.length == 0) {
+      System.out.println("no arguments were given.");
+    } else {
+
+      files = new File[args.length];
+      for (int i = 0; i <= args.length - 1; i++) {
+        try {
+          ClassLoader classLoader = getClass().getClassLoader();
+          files[i] = new File(Objects.requireNonNull(classLoader.getResource(args[i])).getFile());
+        } catch (NullPointerException nullPointerException) {
+          System.err.println("src/" + args[i] + "Is not a valid file");
+        }
+      }
+    }
+  }
+
+  public LexicalAnalyzer[] analyzeFiles() {
+    LexicalAnalyzer[] analyzers = new LexicalAnalyzer[this.files.length];
+    for (int i = 0; i <= this.files.length - 1; i++) {
+      analyzers[i] = new LexicalAnalyzer(files[i]);
+    }
+    return analyzers;
+  }
 }
