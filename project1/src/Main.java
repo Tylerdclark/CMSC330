4f0e3b74a9b3936d0fec975834ae34d28a561832
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-12 19:44:19 -0400

    Looks ugly for now

--- project1/src/Main.java
+++ project1/src/Main.java
@@ -1,12 +1,19 @@
 
+import javax.swing.*;
 import java.io.IOException;
 
 class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, SyntaxError {
 
     FileHandler handler = new FileHandler(args);
-    LexicalAnalyzer[] lyzers = handler.analyzeFiles();
-    for (LexicalAnalyzer lyzer : lyzers) lyzer.getNext();
+    LexicalAnalyzer[] analyzers = handler.analyzeFiles();
+        for (LexicalAnalyzer analyzer : analyzers) {
+          Parser parser = new Parser(analyzer);
+            JFrame result = parser.run();
+            result.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            result.setVisible(true);
+      }
   }
+
 }
