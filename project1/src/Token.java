750c5e8b6636a909d4c1eb8a502020b24f1e065a
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-11 21:32:49 -0400

    Think I am going to go through and make a function for each enum

--- project1/src/Token.java
+++ project1/src/Token.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public enum Token {
+public enum Token {
+
+    STRING{
+        boolean compare(int value){
+            return true;
+        }
+    }, NUMBER, END, BUTTON,  FLOW, GRID, GROUP, LABEL, LAYOUT, PANEL, RADIO, TEXTFIELD, WINDOW,
+    COMMA, COLON, SEMICOLON, PERIOD, LEFT_PAREN, RIGHT_PAREN,
+     EOF
 }
