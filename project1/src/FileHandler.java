749a521b69be9a79ba86ea6501f0b110b33017f8
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-13 21:54:54 -0400

    Look how I massacred my boy

--- project1/src/FileHandler.java
+++ project1/src/FileHandler.java
@@ -1,33 +1,70 @@
+import javax.swing.*;
 import java.io.File;
-import java.util.Objects;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileHandler {
 
-  private File[] files;
+    private File[] files;
+    private LexicalAnalyzer[] analyzers;
+    private JFrame[] jFrames;
 
-  public FileHandler(String[] args) {
+    public FileHandler(String[] args) {
 
-    if (args.length == 0) {
-      System.out.println("no arguments were given.");
-    } else {
+        if (args.length == 0) {
+            System.out.println("no arguments were given.");
+        } else {
+            files = new File[args.length];
+            for (int i = 0; i <= args.length - 1; i++) {
+                try {
+                    // for some reason, my ide thinks the working directory is the directory above this one
+                    Path current = Paths.get("project1/src/"+args[i]);
 
-      files = new File[args.length];
-      for (int i = 0; i <= args.length - 1; i++) {
-        try {
-          ClassLoader classLoader = getClass().getClassLoader();
-          files[i] = new File(Objects.requireNonNull(classLoader.getResource(args[i])).getFile());
-        } catch (NullPointerException nullPointerException) {
-          System.err.println(args[i] + " Is not a valid file");
+                    // so for the final program, use below
+                    //Path current = Paths.get(args[i]);
+
+                    String s = current.toAbsolutePath().toString();
+                    files[i] = new File(s);
+                } catch (NullPointerException nullPointerException) {
+                    System.err.println(args[i] + " Is not a valid file");
+                }
+            }
         }
-      }
     }
-  }
 
-  public LexicalAnalyzer[] analyzeFiles() {
-    LexicalAnalyzer[] analyzers = new LexicalAnalyzer[this.files.length];
-    for (int i = 0; i <= this.files.length - 1; i++) {
-      analyzers[i] = new LexicalAnalyzer(files[i]);
+    public void analyzeFiles() {
+        analyzers = new LexicalAnalyzer[this.files.length];
+        for (int i = 0; i <= this.files.length - 1; i++) {
+            try {
+                analyzers[i] = new LexicalAnalyzer(files[i]);
+            } catch (FileNotFoundException e) {
+                System.err.println(files[i] + " Is not a valid file");
+            }
+        }
     }
-    return analyzers;
-  }
+
+    public void passAnalyzers() {
+        jFrames = new JFrame[analyzers.length];
+        for (int i = 0; i <= analyzers.length - 1; i++) {
+            JFrame result;
+            try {
+                Parser parser = new Parser(analyzers[i]);
+                result = parser.run();
+                jFrames[i] = result;
+            } catch (IOException ioException) {
+                System.err.println("Issue with StreamTokenizer");
+            } catch (SyntaxError syntaxError) {
+                System.err.println(syntaxError.getMessage());
+            }
+        }
+    }
+
+    public void runGUIs() {
+        for (JFrame frame : jFrames) {
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        }
+    }
 }
