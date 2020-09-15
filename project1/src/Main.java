e88c138dfd3217f010fcbf7156b19ffe19b80a04
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-14 21:34:24 -0400

    Just documentation. I can't imagine much more to change here.

--- project1/src/Main.java
+++ project1/src/Main.java
@@ -1,10 +1,29 @@
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Main - Project 1
+ * COURSE: CMSC 330
+ * DATE: 14 SEP 2020
+ * *****************************************************************************
+ */
+
+/**
+ * The Main class provides a command line launch of the parser. This class consists of the public
+ * static void main entry point.
+ *
+ * @author tylerclark
+ */
 class Main {
 
+    /**
+     * Entry point
+     *
+     * @param args names of the input files from where the GUIs will be constructed based on on the
+     *     syntax contained.
+     */
     public static void main(String[] args) {
 
         FileHandler handler = new FileHandler(args);
-        handler.analyzeFiles();
-        handler.passAnalyzers();
         handler.runGUIs();
     }
 }
