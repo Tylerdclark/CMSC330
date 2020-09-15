aead108c8547f0cac782b0ab2b4602c694a0d795
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-14 21:35:24 -0400

    Documentation

--- project1/src/Token.java
+++ project1/src/Token.java
@@ -1,3 +1,18 @@
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: Token - Project 1
+ * COURSE: CMSC 330
+ * DATE: 14 SEP 2020
+ * *****************************************************************************
+ */
+
+/**
+ * An enumerated type used by the {@link LexicalAnalyzer} and the {@link Parser}. Establishes the
+ * tokens to be used in the parsing of GUIs.
+ *
+ * @author tylerclark
+ */
 public enum Token {
     STRING,
     NUMBER,
