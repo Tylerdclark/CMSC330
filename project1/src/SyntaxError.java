3e5ee4f29bc4923376832c90df25b9a4cab77075
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-14 21:35:10 -0400

    Documentation

--- project1/src/SyntaxError.java
+++ project1/src/SyntaxError.java
@@ -1,8 +1,29 @@
-public class SyntaxError extends Exception{
+/*
+ * *****************************************************************************
+ * NAME: Tyler D Clark
+ * PROJECT: SyntaxError - Project 1
+ * COURSE: CMSC 330
+ * DATE: 14 SEP 2020
+ * *****************************************************************************
+ */
 
+/**
+ * Class contains a constructor for the custom Syntax error, which is to be called when the GUI
+ * grammar is incorrect.
+ *
+ * @author tylerclark
+ */
+public class SyntaxError extends Exception {
 
-    SyntaxError(int line, String description){
-        super("Line: " + line + " " + description);
+    /**
+     * Constructor for the custom Syntax error. To be called when the GUI grammar supplied is
+     * incorrect.
+     *
+     * @param description A description of the Syntax error. Describe what was expected and what was
+     *     given
+     * @param line The line on which the SyntaxError was created
+     */
+    SyntaxError(int line, String description) {
+        super(description + " On line: " + line);
     }
-
 }
