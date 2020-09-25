47b20b9ef1c0c0303290391774c7da2d60fafd04
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-09-25 15:14:30 -0400

    barebones starter for the project

---
+++ project2/add.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tyler Clark on 9/25/20.
+//
+#include <iostream>
+#include "add.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ project2/add.h
@@ -0,0 +1,11 @@
+//
+// Created by Tyler Clark on 9/25/20.
+//
+
+#ifndef PROJECT2_ADD_H
+#define PROJECT2_ADD_H
+
+int add(int a, int b);
+
+
+#endif //PROJECT2_ADD_H
