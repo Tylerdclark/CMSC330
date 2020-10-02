62d96b8458c1e151fe88d8ea7cd0be4e4c7a82b9
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-01 22:40:21 -0400

    will use this style of comments

--- project2/src/parse.cpp
+++ project2/src/parse.cpp
@@ -1,6 +1,11 @@
-//
-// Created by Tyler Clark on 9/26/20.
-//
+/**
+    CS-11 Asn 2
+    checks.cpp
+    Purpose: Calculates the total of 6 checks
+
+    @author Tyler Clark
+    @version 1.1 4/10/16
+*/
 
 #include <cctype>
 #include <iostream>
@@ -9,6 +14,14 @@ using namespace std;
 
 #include "parse.h"
 
+
+/**
+    Sample summary
+
+    @param digit the single digit to encode.
+    @return a bar code of the digit using "|" as the long
+    bar and "," as the half bar.
+*/
 string parseName()
 {
     char alnum;
