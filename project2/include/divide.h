4608c0591b1254f34bda8c44e89749d64dc6663f
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-05 22:09:53 -0400

    save from division by zero

--- project2/include/divide.h
+++ project2/include/divide.h
@@ -5,15 +5,16 @@
 #ifndef PROJECT2_DIVIDE_H
 #define PROJECT2_DIVIDE_H
 
-class Divide: public SubExpression
-{
+class Divide : public SubExpression {
 public:
-    Divide(Expression* left, Expression* right): SubExpression(left, right)
-    {
+    Divide(Expression *left, Expression *right) : SubExpression(left, right) {
     }
-    int evaluate()
-    {
-        return left->evaluate() / right->evaluate();
+
+    int evaluate() override {
+        if (right->evaluate() != 0) {
+            return left->evaluate() / right->evaluate();
+        }
+        return 0;
     }
 };
 
