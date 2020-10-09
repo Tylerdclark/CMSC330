4b507c4476f599e7dacd7f1a455c8bde45f6a5b7
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-08 23:30:49 -0400

    dont like using namespace if dont need

--- project2/src/symboltable.cpp
+++ project2/src/symboltable.cpp
@@ -5,16 +5,17 @@
 
 #include <vector>
 
-using namespace std;
 
 #include "symboltable.h"
 
-void SymbolTable::insert(string variable, double value) {
+void SymbolTable::insert(std::string variable, double value)
+{
     const Symbol &symbol = Symbol(variable, value);
     elements.push_back(symbol);
 }
 
-double SymbolTable::lookUp(string variable) const {
+double SymbolTable::lookUp(std::string variable) const
+{
     for (int i = 0; i < elements.size(); i++)
         if (elements[i].variable == variable)
             return elements[i].value;
