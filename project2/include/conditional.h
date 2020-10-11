8c45493d45f9676a7917518871ad524147b09d96
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-10-10 23:15:23 -0400

    new conditional

--- project2/include/conditional.h
+++ project2/include/conditional.h
@@ -5,4 +5,18 @@
 #ifndef PROJECT2_CONDITIONAL_H
 #define PROJECT2_CONDITIONAL_H
 
+class Conditional: public SubExpression
+{
+public:
+    Conditional(Expression* left, Expression* right, Expression* condition): SubExpression(left, right, condition)
+    {
+    }
+    int evaluate() override
+    {
+        if (condition->evaluate()) {
+            return left->evaluate();
+        }
+        return right->evaluate();
+    }
+};
 #endif //PROJECT2_CONDITIONAL_H
